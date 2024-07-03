fb262b87f7bd908dbbcf4782a5bbad24066950c9
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2024-07-03 15:13:18 +0300

    done hook-linked to a DOM element lesson

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,30 @@
+import { useRef, useEffect, useState } from "react";
+
 import Page from "./component/page";
-import PostList from "./container/post-list";
+// import PostList from "./container/post-list";
 
 function App() {
+  const scrollPositionRef = useRef(0);
+
+  const handleScroll = () => {
+    scrollPositionRef.current = window.scrollY + 1;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("scrollPositionRef", scrollPositionRef);
+  }, [window.scrollY]);
+
   return (
     <Page>
-      <PageList />
+      <button onClick={handleScroll}>Click</button>
+      <p style={{height: 10000}}></p>
     </Page>
   );
 }
